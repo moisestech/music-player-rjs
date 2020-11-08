b1ea992a9c104aacabde81f0f6764951ff15a727
Moises Sanabria <m@moises.tech>
2020-11-08 18:45:45 -0500

    init func comp app start Music Player React JS App

---
+++ src/app/index.js
@@ -0,0 +1,15 @@
+import "./app.css";
+import { useState, useEffect } from "react";
+
+export default function App() {
+
+  // load environment connection with Alan AI
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Music Player React JS</h1>
+    </div>
+  );
+}
