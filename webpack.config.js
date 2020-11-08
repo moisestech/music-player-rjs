adefc4eb785c458878d32500228e97404c7568f6
Moises Sanabria <m@moises.tech>
2020-11-08 18:45:13 -0500

    require path webpack dotenv HtmlPlugin CopyPlugin

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|jpe?g|gif)$/i, use: ["file-loader"] },
+    ],
+  },
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./public/_redirects" }],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.config().parsed),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
